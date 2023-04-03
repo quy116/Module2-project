254404a92f469bc03461917bfc55685011629b08
quy116 <nguyenphucquy1106@gmail.com>
2023-04-03 11:35:34 +0700

    hello

---
+++ src/ss1_Introduction/bai_tap/HienThi.java
@@ -0,0 +1,12 @@
+package ss1_Introduction.bai_tap;
+
+import java.util.Scanner;
+
+public class HienThi {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter your name :");
+        String name = sc.nextLine();
+        System.out.println("xin chao " + name);
+    }
+}
