5b919937ecbc16331eca3fc29880f4b1c32a3e02
quy116 <nguyenphucquy1106@gmail.com>
2023-04-04 20:07:06 +0700

    hello

---
+++ src/ss3_array_java/DemKyTu.java
@@ -0,0 +1,24 @@
+package ss3_array_java;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class DemKyTu {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("nhập vào một chuỗi kí tự");
+        String str = sc.nextLine();
+        String[] strArr = str.split("");
+        System.out.println(Arrays.toString(strArr));
+        System.out.println("nhập vào ký tự cần tìm");
+        String str1 = sc.nextLine();
+        int sum = 0;
+        for (int i = 0; i < strArr.length; i++) {
+              if (strArr[i] == str1) {
+                  sum++;
+            }
+        }
+        System.out.println("số lầ kí tự xuất hiền là:" + sum);
+
+    }
+}
--- src/ss3_array_java/TinhDuongCheo.java
+++ src/ss3_array_java/TinhDuongCheo.java
@@ -1,8 +1,9 @@
 package ss3_array_java;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
-public class TimSoNhoNhat {
+public class TinhDuongCheo {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("nhập số hàng của cột");
@@ -10,20 +11,25 @@ public class TimSoNhoNhat {
         System.out.println("nhập số phan tu của cột");
         int col = sc.nextInt();
         int[][] arr = new int[row][col];
-        System.out.println("nhập các phẩn tử của mảng");
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
+        System.out.println("nhap phan tu cua mang");
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
                 arr[i][j] = sc.nextInt();
             }
         }
-        int min = arr[0][0];
+        int sum = 0;
+        int sum2 = 0;
+        int sum3= 0;
+        System.out.println(arr[0][1]);
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[i].length; j++) {
-                if (min > arr[i][j]) {
-                    min = arr[i][j];
+                if (j == sum3) {
+                    sum += arr[i][j];
+                    System.out.println(sum);
                 }
             }
+            sum3++;
         }
-        System.out.println("số nhỏ nhất trong mảng là " + min);
+
     }
 }
